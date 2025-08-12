315450fff130c0b23ebb8066e8928a44f4a67cbc
Akil23-alt <kauaakil23@gmail.com>
2025-08-12 09:02:52 -0300

    Final

---
+++ models/numberModel.js
@@ -0,0 +1,8 @@
+// Por enquanto, este arquivo não é usado, mas já está preparado para crescer
+
+// Exemplo futuro:
+// const mongoose = require('mongoose');
+// const numberSchema = new mongoose.Schema({
+// numeros: [Number]
+// });
+// module.exports = mongoose.model('NumberModel', numberSchema);
